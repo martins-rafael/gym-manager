764ba2a7e3fda739cc49c20033682b79c6cf5ca5
Rafael Martins <rafaeldcmartins@gmail.com>
2020-07-02 21:54:10 -0300

    fix filter state

--- public/scripts.js
+++ public/scripts.js
@@ -36,6 +36,7 @@ function paginate(selectedPage, totalPages) {
 const pagination = document.querySelector('.pagination')
 const page = +pagination.dataset.page
 const total = +pagination.dataset.total
+const filter = pagination.dataset.filter
 const pages = paginate(page, total)
 
 let elements = ''
@@ -44,7 +45,11 @@ for (let page of pages) {
     if (String(page).includes('...')) {
         elements += `<span>${page}</span>`
     } else {
-        elements += `<a href="?page=${page}">${page}</a>`
+        if (filter) {
+            elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+        } else {
+            elements += `<a href="?page=${page}">${page}</a>`
+        }
     }
 }
 
